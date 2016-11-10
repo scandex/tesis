9b2e6040d979925f48f9c1f73dd9ea94f51bb0a7
Sebastian Camilo Arguelles Niño <sebastian_arguelles@hotmail.com>
2016-11-10 15:53:17 +0000

    first release fox

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,5 +1,8 @@
 class Concept
     include Mongoid::Document
     field :id, type: String
+    field :concepto, type: String
+    
     store_in collection: "concepts2"
+    embedded_in :post, :inverse_of => :concepts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,12 @@ class Post
     field :user, type: String
     field :votes, type: String
    field :rank, type: Integer
+   field :verbs, type: Array
+   field :adjs, type: Array
+   field :nouns, type: Array
+   
     
      embeds_many :comments, class_name: "Comment"
+     embeds_many :concepts, class_name: "Concept"
       paginates_per 5
 end
